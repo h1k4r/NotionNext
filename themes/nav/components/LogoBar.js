62ccdcbd2190eb5b98cdc3750a4e025081168f8e
繁花 <60561049+h1k4r@users.noreply.github.com>
2024-12-28 23:06:44 +0800

    Update LogoBar.js

--- themes/nav/components/LogoBar.js
+++ themes/nav/components/LogoBar.js
@@ -12,12 +12,17 @@ export default function LogoBar(props) {
 
   return (
         <div id='top-wrapper' className='w-full flex items-center'>
-                <Link href='/' className='md:w-48 grid justify-items-center text-md md:text-xl dark:text-gray-200'>
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={siteInfo?.icon?.replaceAll('width=400', 'width=280')}
-                        height='44px' alt={siteConfig('AUTHOR') + ' - ' + siteConfig('NEXT_PUBLIC_BIO')} className='md:block transition-all hover:scale-110 duration-150' placeholderSrc='' />
-                    {siteConfig('NAV_SHOW_TITLE_TEXT', null, CONFIG) && siteConfig('TITLE')}
-                </Link>
+<Link href='/' className='md:w-48 grid justify-items-center text-md md:text-xl dark:text-gray-200'>
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img src={siteInfo?.icon?.replaceAll('width=400', 'width=280')}
+        height='44px' 
+        width='44px'
+        alt={siteConfig('AUTHOR') + ' - ' + siteConfig('NEXT_PUBLIC_BIO')} 
+        className='md:h-[120px] md:w-[120px] h-[44px] w-[44px] rounded-full md:block transition-all hover:scale-110 duration-150' 
+        placeholderSrc='' />
+    {siteConfig('NAV_SHOW_TITLE_TEXT', null, CONFIG) && siteConfig('TITLE')}
+</Link>
+
         </div>
   )
 }
