b68191a3a13793c21f6220f45a29ef54931c03bd
繁花 <60561049+h1k4r@users.noreply.github.com>
2025-01-01 11:35:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,5 @@
-// 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 const BLOG = {
-LINK: process.env.NEXT_PUBLIC_LINK || 'https://awaink
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://awa.ink'
 }
 
 module.exports = BLOG
